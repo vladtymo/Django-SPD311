f31f306774aceb81757aa557194d4786b5eb0604
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-06-07 14:11:25 +0300

    Create product pages

---
+++ products/views.py
@@ -0,0 +1,21 @@
+from itertools import count
+from django.shortcuts import render
+
+
+def index(request):
+    # products = Product.objects.all()
+    return render(request, "index.html")
+
+
+def catalog(request):
+    # products = Product.objects.all()
+
+    products = [
+        {"name": "Product 1", "price": 10.99},
+        {"name": "Product 2", "price": 12.99},
+        {"name": "Product 3", "price": 15.99},
+    ]
+
+    return render(
+        request, "catalog.html", {"products": products, "count": len(products)}
+    )
