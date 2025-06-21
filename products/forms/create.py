ecbac9c77ce44b196e72c9ed24aa46cb50166456
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-06-21 12:36:50 +0300

    Form widgets

--- products/forms/create.py
+++ products/forms/create.py
@@ -3,7 +3,11 @@ from products.models import Product
 
 
 class CreateProduct(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = "__all__"  # all User model fields
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter product name"}
+            ),
+        }
